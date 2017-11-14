6488c2c078d097257ec871acd95c8934307bb3dc
Nolan Hughes <nhughes987@gmail.com>
2017-11-14 21:38:08 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,27 +1,27 @@
 # class Appointment
 #   attr_accessor :date, :doctor, :patient
-# 
+#
 #   def initialize(date, doctor)
 #     @date = date
 #     @doctor = doctor
 #     @appointments = []
-# 
+#
 #   end
-# 
+#
 #   def add_appointment(appointment)
 #     @appointments << appointment
 #     self.patient
 #   end
-# 
+#
 #   def patient
 #     @patient
 #   end
-# 
+#
 #   def doctor
 #     @doctor = doctor
 #     doctor.add_appointment(self)
 #   end
-# 
+#
 # end
 
 class Song
