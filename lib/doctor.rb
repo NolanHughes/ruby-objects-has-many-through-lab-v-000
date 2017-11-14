45149a1153bd65396748438f608d77b7b0e437af
Nolan Hughes <nhughes987@gmail.com>
2017-11-14 21:41:25 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,24 +1,24 @@
 # class Doctor
 #   attr_accessor :name, :appointments, :patient
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @appointments = []
 #   end
-# 
+#
 #   def add_appointment(appointment)
 #     @appointments << appointment
 #     appointment.doctor = self
 #   end
-# 
+#
 #   def appointments
 #     @appointments
 #   end
-# 
+#
 #   def patients
 #     self.appointments.collect { |app_object| app_object.patient }
 #   end
-# 
+#
 # end
 
 class Artist
--- lib/patient.rb
+++ lib/patient.rb
@@ -19,3 +19,24 @@
 #     self.appointments.collect { |app| app.doctor }
 #   end
 # end
+
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appointment)
+    @appointment << appointment
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    self.appointments.collect { |app_objects| app_objects.doctors }
+  end
+end
